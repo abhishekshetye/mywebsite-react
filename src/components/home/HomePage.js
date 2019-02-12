ebf9d54cf27df0fdcbed5daadfd3cf2d11adf3f1
abhishekshetye <abhishekshetye@ves.ac.in>
2019-02-12 23:36:57 +0530

    Add projects page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,8 +4,9 @@ import {Link} from 'react-router';
 class HomePage extends React.Component {
 	render(){
 		return (
-			<div className="jumbotron">
-				<h1>Welcome</h1>
+			<div className="jumbotron" id="title-container">
+				<h1>Abhishek Shetye</h1>
+				<Link to="projects">Projects</Link>
 			</div>
 		);
 	}
---
+++ src/components/projects/ProjectsComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class ProjectsComponent extends React.Component {
+	render(){
+		return (
+			<div className="container" id="title-container">
+				<h1>My Projects</h1>
+			</div>
+		);
+	}
+}
+
+export default ProjectsComponent;
